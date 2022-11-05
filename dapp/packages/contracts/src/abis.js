caa859286d6a0e4bc6c6c642f43122b26e5f74b4
Mert Akyazı <mert@Merts-MacBook-Pro.local>
2022-11-05 17:15:12 +0300

    Initialized working repo

---
+++ dapp/packages/contracts/src/abis.js
@@ -0,0 +1,13 @@
+import erc20Abi from "./abis/erc20.json";
+import ownableAbi from "./abis/ownable.json";
+import makalelikAbi from "./abis/makalelik.json";
+import abonmanSozlesmesiAbi from "./abis/abonmanSozlesmesi.json";
+
+const abis = {
+  erc20: erc20Abi,
+  ownable: ownableAbi,
+  makalelik: makalelikAbi,
+  abonmanSozlesmesi: abonmanSozlesmesiAbi
+};
+
+export default abis;
---
+++ dapp/packages/contracts/src/addresses.js
@@ -0,0 +1,5 @@
+// This address points to a dummy ERC-20 contract. Replace it with your own smart contracts.
+const addresses = {
+  aboneErc20: "0xDaC819A2A27781a68EE660187f37885796c2cB6E",
+};
+export default addresses;
